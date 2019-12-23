a47a3548e6e782f0be00af72d09e9462aa0eb5bf
TripleThreads <segnih8@gmail.com>
2019-12-23 19:06:17 +0300

    find duplicate number

---
+++ leetcode/binary search/find-duplicate-number.cpp
@@ -0,0 +1,29 @@
+//
+// Created by segni on 23/12/2019.
+//
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+        int left = 1, right = nums.size() - 1;
+
+        while(left < right){
+
+            int mid = left + (right - left) / 2;
+            int count = 0;
+
+            for(int num: nums){
+                if(num <= mid) count++;
+            }
+
+            if(count > mid)
+                right = mid;
+
+            else
+                left = mid + 1;
+        }
+
+        return right;
+    }
+};
