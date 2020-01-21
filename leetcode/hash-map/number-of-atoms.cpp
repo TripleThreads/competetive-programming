c252d7c03111df0145a93192db1b90e16f1e4fc5
TripleThreads <segnih8@gmail.com>
2020-01-21 23:41:37 +0300

    updated number of atoms

--- leetcode/hash-map/number-of-atoms.cpp
+++ leetcode/hash-map/number-of-atoms.cpp
@@ -4,7 +4,7 @@
 
 class Solution {
 public:
-    int getDigit(string s, int i) {
+    int getDigit(string s, int i) { // get digit starting from the last term
         int j = i;
         while (i < s.length() && isdigit(s.at(i)))
             i--;
@@ -12,36 +12,29 @@ public:
     }
     string countOfAtoms(string formula) {
         map<string, int> count;
-        stack<int> stck;
-        stck.push(1);
-
+        stack<int> subs; // holds subscripts
+        subs.push(1);
         int n = formula.size(), d = 1;
         for (int i = n - 1; i >= 0; i--) {
-
             char c = formula.at(i);
-
             if (c == ')')
-                stck.push(d * stck.top());
+                subs.push(d * subs.top());
 
             else if (c == '(')
-                stck.pop();
+                subs.pop();
 
             else if (isdigit(c)) {
                 d = getDigit(formula, i);
-                i -= log10(d) - 1;
+                i -= log10(d) - 1; // if the number is more than one digit
                 continue;
             }
-
-            else if (islower(c))
-                count[formula.substr(--i, 2)] += d * stck.top();
-
-            else
-                count[formula.substr(i, 1)] += d * stck.top();
-
+            else if (islower(c)) // if it is lowercase it will take the preceding char too. ex. Br
+                count[formula.substr(--i, 2)] += d * subs.top();
+            else // if it is upper case simply increment ex. H
+                count[formula.substr(i, 1)] += d * subs.top();
             d = 1;
         }
         string result;
-
         for (auto const & [k, v] : count) {
             result.append(k);
             result += v > 1 ? to_string(v) : "";
