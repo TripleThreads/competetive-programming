d7d3048e3be03f27d86efb6036f5dff05e582ae1
TripleThreads <segnih8@gmail.com>
2020-02-05 11:49:28 +0300

    unique paths

--- leetcode/dp/unique-paths.cpp
+++ leetcode/dp/unique-paths.cpp
@@ -7,14 +7,12 @@ public:
     int uniquePaths(int m, int n) {
 
         int dp[n][m];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             dp[i][0] = 1;
-            dp[i][m - 1] = 1;
-        }
-        for (int i = 0; i < m; i++) {
+
+        for (int i = 0; i < m; i++)
             dp[0][i] = 1;
-            dp[n - 1][i] = 1;
-        }
+
         for (int i = 1; i < n; i++)
 
             for (int j = 1; j < m; j++)
