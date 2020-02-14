edad4f5471adce47978ecf30112abfbc77c6746e
TripleThreads <segnih8@gmail.com>
2020-02-14 17:20:45 +0300

    fixed

--- leetcode/dfs && bfs/loud-and-rich.cpp
+++ leetcode/dfs && bfs/loud-and-rich.cpp
@@ -4,17 +4,17 @@
 
 class Solution {
 public:
+    int dfs(int index, vector<int>& quiet, unordered_map<int, vector<int>> &adj, vector<int> &memo) {
+        if (memo[index] >= 0)
+            return memo[index];
 
-    vector<int> memo;
-    int dfs(int i, vector<int>& quiet, unordered_map<int, vector<int>> &adj) {
-        if (memo[i] >= 0) return memo[i];
-        memo[i] = i;
-        for (int j : adj[i]) {
-            if (quiet[memo[i]] > quiet[dfs(j, quiet, adj)])  {
-                memo[i] = memo[j];
+        memo[index] = index;
+        for (int i : adj[index]) {
+            if (quiet[memo[index]] > quiet[dfs(i, quiet, adj, memo)])  {
+                memo[index] = memo[i];
             }
         }
-        return memo[i];
+        return memo[index];
     }
 
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
@@ -22,9 +22,9 @@ public:
         for (auto v : richer)
             adj[v[1]].push_back(v[0]);
 
-        memo = vector<int>(quiet.size(), INT_MIN);
+        vector<int> memo(quiet.size(), INT_MIN);
         for (int i = 0; i < quiet.size(); i++) {
-            dfs(i, quiet, adj);
+            dfs(i, quiet, adj, memo);
         }
         return memo;
     }
