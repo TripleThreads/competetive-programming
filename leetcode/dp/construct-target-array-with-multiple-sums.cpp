5567d5ba2ec2a35821366287e0403b2e395625c1
Segni Habulu <programmers2017@outlook.com>
2020-02-17 22:16:21 -0900

    Update construct-target-array-with-multiple-sums.cpp

--- leetcode/dp/construct-target-array-with-multiple-sums.cpp
+++ leetcode/dp/construct-target-array-with-multiple-sums.cpp
@@ -1,50 +1,16 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& target) {
-        int n = target.size();
-        if (n == 1) {
-            return true;
+   bool isPossible(vector<int>& target) {
+        auto sum = accumulate(begin(target), end(target), (long long)0);
+        priority_queue<int> que(begin(target), end(target));
+        while (sum > 1 && que.top() > sum / 2) {
+            auto cur = que.top(); 
+            que.pop();
+            auto prev = cur % (sum - cur);
+            que.push(prev);
+            sum -= cur - prev;
+            cout << cur << " " << prev << " " << s << endl;
         }
-        int maxVal = 0;
-        unordered_set<int> visited;
-        for (int i : target) {
-            if (i != 1 && visited.count(i))
-                return false;
-            
-            visited.insert(i);
-            if (i > maxVal) {
-                maxVal = i;
-            }
-        }
-        int count = visited.size();
-        unsigned long long int a = 1, b = n, c = n + (n - 1);
-        if (visited.count(a))
-            count--;
-        if (visited.count(b))
-            count--;
-        if (visited.count(c))
-            count--;
-        if (count == 0)
-            return true;
-        
-        vector<int> vec;
-        for (int i : visited) {
-            vec.push_back(i);
-        }
-        
-        for (int i = 0; i < vec.size(); i++) {
-            if (vec[i] < c)
-                continue;
-            if ((vec[i] - n) % (n - 1) != 0) {
-                return false;
-            }
-            unsigned long long int desired = (vec[i] - n) / (n - 1);
-            if (!(visited.count(vec[i] - (n - 1)) && visited.count(vec[i] - 2 * (n - 1)))) {
-                count--;
-            }
-            if (count == 0)
-                return true;
-        }
-        return false;
+        return sum <= target.size();
     }
 };
