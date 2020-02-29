e98f3dbd38c229ce6a12d7b334558a466d6e68e2
TripleThreads <segnih8@gmail.com>
2020-02-29 15:59:50 +0300

    updated

--- leetcode/greedy/couples-holding-hands.cpp
+++ leetcode/greedy/couples-holding-hands.cpp
@@ -10,7 +10,7 @@ public:
         for (int i = 0; i < n - 1; i++) {
             int firstLover = row[i] / 2;
             for (int j = 0; j < n; j++) {
-                if (i != j && row[j] / 2 == firstLover && abs(j - i) > 1) {
+                if (row[j] / 2 == firstLover && abs(j - i) > 1) {
                     totalCount++;
                     swap(row[i + 1], row[j]);
                     break;
